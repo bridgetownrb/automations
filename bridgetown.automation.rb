18a0031b6637e8e3091a8c445116ee3beddcb278
Jared White <jared@jaredwhite.com>
2020-05-22 01:31:36 -0700

    Create bridgetown.automation.rb

---
+++ bridgetown.automation.rb
@@ -0,0 +1,2 @@
+say "Howdy! You can go to https://github.com/bridgetownrb/bridgetown-automations to browse the list of automations provided there."
+say "For more information about automations, visit https://www.bridgetownrb.com/docs/automations"
