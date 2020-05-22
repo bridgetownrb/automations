d2718022fa3da51497d3510f132de4feacd3d7b1
Jared White <jared@jaredwhite.com>
2020-05-22 11:04:11 -0700

    Update bridgetown.automation.rb

--- bridgetown.automation.rb
+++ bridgetown.automation.rb
@@ -1,2 +1,2 @@
-say "Howdy! Visit https://github.com/bridgetownrb/bridgetown-automations to browse the list of automations there."
+say "Howdy! Visit https://github.com/bridgetownrb/automations to browse the list of automations there."
 say "For more information about automations, visit https://www.bridgetownrb.com/docs/automations"
